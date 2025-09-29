a6ade8aeb4756a97bd9934a8180b64d8e3a86ff8
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2025-09-29 18:04:36 +0200

    general update: now using port 80

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api")
+def api():
+    return render_template("api.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
